ea85dd89b3c578d23fdc4ef4cd67f8b0d214ada6
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2023-11-18 04:27:27 +0600

    [refactor](remove): remove redundant lines

--- example/auto_fail_sample_with_logs/entrypoint.sh
+++ example/auto_fail_sample_with_logs/entrypoint.sh
@@ -1,25 +1,7 @@
 #!/bin/sh
 
-# # Function to log messages
-# log_message() {
-#     timestamp=$(date +"%Y-%m-%d %T")
-#     echo "$timestamp $1" >> /app/container.log
-# }
-
-# # Log a message every second for 5 minutes
-# for i in {1..300}; do
-#     log_message "Log message $i"
-#     sleep 1
-# done
-
-# # Fail with an error message
-# log_message "Container failed intentionally"
-# exit 1
-
-#!/bin/sh
-
-# Log messages every second for 5 minutes
-for i in {1..300}; do
+# Log messages 
+for i in {1..1000}; do
     echo "Log message $i"
     sleep 1
 done
